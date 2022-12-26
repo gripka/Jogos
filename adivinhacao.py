f4f629a9aa98a998df0671ac15959a17dd0d5032
Gabriel Ripka <ga_gripka@hotmail.com>
2022-12-26 17:37:21 -0300

    Adivinhação

---
+++ adivinhacao.py
@@ -0,0 +1,53 @@
+#print("bah", "meo", sep="\n")
+def jogar():
+    import random
+
+    print("**************")
+    print("* Luix, rsrs *")
+    print("**************")
+
+    #numero_secreto = 5
+    numero_secreto = round(random.randrange(1, 101))
+    total_de_tentativas = 0
+    #pontos = 1000
+    rodada = 1
+    print(numero_secreto)
+
+    print("Escolha a dificuldade")
+    print("(1) Facil (2) Médio (3) Difícil")
+    nivel = int(input("Digite o nível de dificuldade escolhido: "))
+    if(nivel == 1):
+        total_de_tentativas = 20
+    elif(nivel == 2):
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 1
+
+    while (rodada <= total_de_tentativas):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        chute_str = input("Digite o seu número: ")
+        print("Você digitou " , chute_str)
+        chute = int(chute_str)
+
+        if (chute < 1 or chute > 100):
+            print("Só é valido números de 1 a 100")
+            continue
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+
+        if(acertou):
+            print("Parabéns! Você acertou!")
+            break
+        else:
+            if(maior):
+                print("O seu chute foi maior do que o número secreto!")
+            elif(menor):
+                print("O seu chute foi menor do que o número secreto!")
+        rodada = rodada +1
+
+    #pontos_perdidos = numero_secreto - chute
+    #pontos = pontos - pontos_perdidos
+
+    print("Fim do jogo")
